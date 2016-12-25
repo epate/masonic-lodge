5c1228a57e90ccdee8a02836a2b5c45a31d6bf98
Emmett Pate <emmett@epate.com>
2016-12-25 09:56:30 -0500

    removed old code

--- cgi-bin/deanimate.php
+++ cgi-bin/deanimate.php
@@ -3,9 +3,6 @@ $file = $_SERVER[DOCUMENT_ROOT] . "/museum/newsletters/previews/" . $_GET['p'];
 $image = @imagecreatefromgif($file);
 $cropped = imagecreatetruecolor(300,300);
 imagecopy($cropped, $image, 0, 0, 0, 0, 300, 300);
-
-// header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-// header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header('Content-Type: image/jpeg');
 imagejpeg($cropped);
 ?>
